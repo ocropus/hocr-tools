c167b92deb7353d2ce149c0d5b6590aaaea7be51
Konstantin Baierer <konstantin.baierer@bib.uni-mannheim.de>
2016-03-01 15:46:57 +0100

    Basic setuptools support

---
+++ setup.py
@@ -0,0 +1,9 @@
+import glob
+from setuptools import setup
+setup(
+    name = "hocr_tools",
+    version = "0.1",
+    author = 'Thomas Breuel',
+    description = 'Advanced tools for hOCR integration',
+    scripts = [c for c in glob.glob("hocr-*")]
+)
