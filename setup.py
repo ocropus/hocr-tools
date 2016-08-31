6ad9190affa47cdce91b5b5ce50075b0bde00cf4
Konstantin Baierer <unixprog@gmail.com>
2016-09-01 00:00:16 +0200

    Add/extend setup.cfg/setup.py for release on PyPI

--- setup.py
+++ setup.py
@@ -4,8 +4,22 @@ import glob
 from setuptools import setup
 setup(
     name = "hocr_tools",
-    version = "0.1",
-    author = 'Thomas Breuel',
+    version = "1.2.2",
     description = 'Advanced tools for hOCR integration',
+    author = 'Thomas Breuel',
+    author_email = 'tmbdev@gmail.com',
+    url = 'https://github.com/tmbdev/hocr-tools',
+    download_url = 'https://github.com/tmbdev/hocr-tools/tarball/1.2.2',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+        'Topic :: Utilities',
+    ],
     scripts = [c for c in glob.glob("hocr-*")]
 )
