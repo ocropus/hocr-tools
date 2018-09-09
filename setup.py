c153350ed2d9f15ec03d6fc3c641a5c186f9a5d9
Philipp Zumstein <zuphilip@users.noreply.github.com>
2018-09-09 21:49:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Multimedia :: Graphics :: Graphics Conversion',
         'Topic :: Scientific/Engineering :: Image Recognition',
         'Topic :: Utilities',
